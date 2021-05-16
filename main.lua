15c7626e2359f8a5b42a52200ce8ac16509bf393
Sectimus <Sectimus@hotmail.co.uk>
2021-05-16 08:30:32 +0100

    added telescope properties and removed onrender for calc function

--- main.lua
+++ main.lua
@@ -1,23 +1,25 @@
 local mod = RegisterMod("Planetarium Chance Display", 1)
 
-local function onRender(self, t)
-    self:updatePlanetariumChance();
-
-    --planetariumChance = [1, 21 ] --Index for stage is stage-1
+function mod:onRender()
+    if self.currentFloorSpawnChance == nil then
+        Isaac.RenderText(string.format("%.1s%%", "?"), 50, 60, 1, 1, 1, 255);
+    else
+        Isaac.RenderText(string.format("%.1f%%", self.currentFloorSpawnChance), 50, 60, 1, 1, 1, 255);
+    end
 end
 
 function mod:updatePlanetariumChance()
     local game = Game();
     local level = game:GetLevel();
-    local treasurerooms_visited = game:GetTreasureRoomVisitCount();
     local stage = level:GetStage();
-
     --Planetariums can also not normally be encountered after Depths II, though Telescope Lens allows them to appear in  Womb and  Corpse.
-    if stage <= LevelStage.STAGE3_2 or (stage > LevelStage.STAGE3_2 and stage < LevelStage.STAGE5) then --Before Womb or Between Womb/Utero with Telescope Lens
-        local game = Game();
-        local level = game:GetLevel();
-        local stage = level:GetStage();
-        self.currentFloorSpawnChance = 1+(100*(0.2 * (stage - skippedRooms() -1))); --chance before items
+    if (stage <= LevelStage.STAGE3_2) or (stage > LevelStage.STAGE3_2 and stage < LevelStage.STAGE5 and Isaac.GetPlayer():HasTrinket(TrinketType.TRINKET_TELESCOPE_LENS)) then --Before Womb or Between Womb/Utero with Telescope Lens
+        local treasureSkips = skippedRooms();
+        log("----")
+        log(stage)
+        log(treasureSkips)
+        log("----")
+        self.currentFloorSpawnChance = 1+(100*(0.2 * treasureSkips)); --chance before items
 
         --items
         if Isaac.GetPlayer():HasCollectible(CollectibleType.COLLECTIBLE_MAGIC_8_BALL) then
@@ -25,14 +27,14 @@ function mod:updatePlanetariumChance()
         end
         if Isaac.GetPlayer():HasCollectible(CollectibleType.COLLECTIBLE_CRYSTAL_BALL) then
             self.currentFloorSpawnChance = self.currentFloorSpawnChance + 15;
-            
-            --TODO The Crystal Ball bonus is 100% if a Treasure Room has been skipped
+            -- if treasureSkips > 0 then --The Crystal Ball bonus is 100% if a Treasure Room has been skipped (THIS IS FALSE)
+            --     self.currentFloorSpawnChance = 100;
+            -- end
         end
         if Isaac.GetPlayer():HasTrinket(TrinketType.TRINKET_TELESCOPE_LENS) then
             self.currentFloorSpawnChance = self.currentFloorSpawnChance + 9;
 
             --TODO If Isaac enters a Planetarium, the chance will be set to 1% and can be increased only with a Telescope Lens, by 15%.
-            --TODO Planetariums can also not normally be encountered after Depths Depths II, though Telescope Lens allows them to appear in Womb and Corpse.
         end
 
     elseif stage > LevelStage.STAGE3_2 and not Isaac.GetPlayer():HasTrinket(TrinketType.TRINKET_TELESCOPE_LENS) then --After depths2 and no Telescope Lens
@@ -41,17 +43,10 @@ function mod:updatePlanetariumChance()
 
     
 
-    --Planetarium chance can never be less than 1% or more than 100%.
-    if self.currentFloorSpawnChance<1 then
-        self.currentFloorSpawnChance = 1;
-    elseif self.currentFloorSpawnChance>100 then
+    --Planetarium chance can never be more than 100%.
+    if self.currentFloorSpawnChance>100 then
         self.currentFloorSpawnChance = 100;
     end
-
-    
-    Isaac.RenderText(string.format( "%.1f%%", self.currentFloorSpawnChance ), 50, 60, 1, 1, 1, 255);
-
-    
 end
 
 -- Returns the amount of skipped treasure rooms (does not count the current floor room if it has not been entered yet)
@@ -63,6 +58,7 @@ function skippedRooms()
     local stage = level:GetStage();
 
     skippedTreasure = stage - treasurerooms_visited;
+    
 
     local rooms = level:GetRooms();
     for i = 0, #rooms-1 do
@@ -73,6 +69,7 @@ function skippedRooms()
             end
         end
     end
+    log(skippedTreasure);
     return skippedTreasure;
 end
 
@@ -88,5 +85,8 @@ local function test()
 
 end
 
-mod:AddCallback(ModCallbacks.MC_POST_RENDER, onRender);
+mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.updatePlanetariumChance);
+mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.updatePlanetariumChance);
+
+mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.onRender);
 mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, test)

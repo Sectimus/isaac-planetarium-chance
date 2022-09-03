2360aede25d239a35a927bbf55c27af2a473149d
Sectimus <Sectimus@hotmail.co.uk>
2022-09-03 19:24:42 +0100

    removed json as data is no longer needed to be encoded

--- main.lua
+++ main.lua
@@ -1,19 +1,18 @@
 PlanetariumChance = RegisterMod("Planetarium Chance", 1)
 local mod = PlanetariumChance
-local json = require("json")
 
-mod.initialized=false
+mod.initialized = false
 
 function mod:onRender(shaderName)
 	if mod:shouldDeHook() then return end
-	
+
 	local isShader = shaderName == "UI_DrawPlanetariumChance_DummyShader" and true or false
-	
-	if not (Game():IsPaused() and Isaac.GetPlayer(0).ControlsEnabled) and not isShader then return end -- no render when unpaused 
+
+	if not (Game():IsPaused() and Isaac.GetPlayer(0).ControlsEnabled) and not isShader then return end -- no render when unpaused
 	if (Game():IsPaused() and Isaac.GetPlayer(0).ControlsEnabled) and isShader then return end -- no shader when paused
-	
+
 	if shaderName ~= nil and not isShader then return end -- final failsafe
-	
+
 	mod:updateCheck()
 
 	--account for screenshake offset
@@ -24,27 +23,27 @@ function mod:onRender(shaderName)
 	else
 		mod:updatePlanetariumChance()
 	end
-	self.font:DrawString(valueOutput, textCoords.X+16, textCoords.Y+1, KColor(1,1,1,0.5),0,true)
-	self.hudSprite:Render(self.coords, Vector(0,0), Vector(0,0))
+	self.font:DrawString(valueOutput, textCoords.X + 16, textCoords.Y + 1, KColor(1, 1, 1, 0.5), 0, true)
+	self.hudSprite:Render(self.coords, Vector(0, 0), Vector(0, 0))
 
 	--differential popup
-	if self.fontalpha and self.fontalpha>0 then
+	if self.fontalpha and self.fontalpha > 0 then
 		local alpha = self.fontalpha
 		if self.fontalpha > 0.5 then
 			alpha = 0.5
 		end
-		if self.storage.previousFloorSpawnChance == nil then 
+		if self.storage.previousFloorSpawnChance == nil then
 			self.storage.previousFloorSpawnChance = self.storage.currentFloorSpawnChance
 		end
 		local difference = self.storage.currentFloorSpawnChance - self.storage.previousFloorSpawnChance
 		local differenceOutput = string.format("%.1f%%", difference)
-		local slide = TextAcceleration((2.9 - self.fontalpha)/(2 * 0.01))
-		if difference>0 then --positive difference
-			self.font:DrawString("+"..differenceOutput, textCoords.X + 46 + slide, textCoords.Y+1, KColor(0,1,0,alpha),0,true)
-		elseif difference<0 then --negative difference
-			self.font:DrawString(differenceOutput, textCoords.X + 46 + slide, textCoords.Y+1, KColor(1,0,0,alpha),0,true)
+		local slide = TextAcceleration((2.9 - self.fontalpha) / (2 * 0.01))
+		if difference > 0 then --positive difference
+			self.font:DrawString("+" .. differenceOutput, textCoords.X + 46 + slide, textCoords.Y + 1, KColor(0, 1, 0, alpha), 0, true)
+		elseif difference < 0 then --negative difference
+			self.font:DrawString(differenceOutput, textCoords.X + 46 + slide, textCoords.Y + 1, KColor(1, 0, 0, alpha), 0, true)
 		end
-		self.fontalpha = self.fontalpha-0.01
+		self.fontalpha = self.fontalpha - 0.01
 	end
 end
 
@@ -67,11 +66,11 @@ function mod:init(continued)
 				end
 			end
 		end
-		mod:SaveData(json.encode(self.storage.canPlanetariumsSpawn)) -- this is the only thing that needs to be saved, everything else can be recalculated
+		mod:SaveData(self.storage.canPlanetariumsSpawn) -- this is the only thing that needs to be saved, everything else can be recalculated
 	elseif continued then
-		self.storage.canPlanetariumsSpawn = json.decode(mod:LoadData()) or 1
+		self.storage.canPlanetariumsSpawn = mod:LoadData() or 1
 	end
-	
+
 	self.storage.currentFloorSpawnChance = nil
 
 	self:updatePlanetariumChance()
@@ -80,38 +79,38 @@ function mod:init(continued)
 
 	self.hudSprite = Sprite()
 	self.hudSprite:Load("gfx/ui/hudstats2.anm2", true)
-	self.hudSprite.Color = Color(1,1,1,0.5)
+	self.hudSprite.Color = Color(1, 1, 1, 0.5)
 	self.hudSprite:SetFrame("Idle", 8)
 	self.font = Font()
 	self.font:Load("font/luaminioutlined.fnt")
-	
+
 	self.initialized = true
 end
 
 -- update on new level
 function mod:updatePlanetariumChance()
-	
+
 	local level = Game():GetLevel()
- 
+
 	self.storage.previousFloorSpawnChance = self.storage.currentFloorSpawnChance
-	
+
 	self.storage.currentFloorSpawnChance = level:GetPlanetariumChance()
-	
-	if self.storage.currentFloorSpawnChance>1 then
+
+	if self.storage.currentFloorSpawnChance > 1 then
 		self.storage.currentFloorSpawnChance = 1
-	elseif self.storage.currentFloorSpawnChance<0 then 
+	elseif self.storage.currentFloorSpawnChance < 0 then
 		self.storage.currentFloorSpawnChance = 0
 	end
-	
+
 	if level:IsAscent() or self.storage.canPlanetariumsSpawn == 0 then
 		self.storage.currentFloorSpawnChance = 0
 	end
-		
+
 	--make absolute
 	self.storage.currentFloorSpawnChance = self.storage.currentFloorSpawnChance * 100
 
 	--don't display popup if there is no change or if run new/continued
-	if self.storage.previousFloorSpawnChance and (self.storage.currentFloorSpawnChance ~= self.storage.previousFloorSpawnChance ) then
+	if self.storage.previousFloorSpawnChance and (self.storage.currentFloorSpawnChance ~= self.storage.previousFloorSpawnChance) then
 		self.fontalpha = 2.9
 	end
 end
@@ -138,20 +137,20 @@ function mod:updatePosition()
 	local RedHeartShift = false
 	local SoulHeartShift = false
 	local DualityShift = false
-	
+
 	local ShiftCount = 0
 
 	self.coords = Vector(0, 168)
-	
+
 	for i = 0, Game():GetNumPlayers() - 1 do
 		local player = Isaac.GetPlayer(i)
 		local playerType = player:GetPlayerType()
-		
+
 		if player:GetBabySkin() == -1 then
 			if i > 0 and player.Parent == nil and playerType == player:GetMainTwin():GetPlayerType() and not TrueCoopShift then
 				TrueCoopShift = true
 			end
-				
+
 			if playerType ~= PlayerType.PLAYER_BLUEBABY_B and not BombShift then -- Shift Stats because of Bomb Counter
 				BombShift = true
 			end
@@ -187,13 +186,13 @@ function mod:updatePosition()
 	if ShiftCount > 0 then
 		self.coords = self.coords + Vector(0, (11 * ShiftCount) - 2)
 	end
-	
+
 	--For some reason whether or not Jacob&Esau are 1st player or another player matters, so I have to check specifically if Jacob is player 1 here
 	if Isaac.GetPlayer(0):GetPlayerType() == PlayerType.PLAYER_JACOB then
 		self.coords = self.coords + Vector(0, 30)
 	elseif TrueCoopShift then
 		self.coords = self.coords + Vector(0, 16)
-		if DualityShift then 
+		if DualityShift then
 			self.coords = self.coords + Vector(0, -2) -- I hate this
 		end
 	end
@@ -211,11 +210,11 @@ end
 
 function mod:updateCheck()
 	local updatePos = false
-	
+
 	local activePlayers = Game():GetNumPlayers()
-	
+
 	for p = 1, activePlayers do
-		local player = Isaac.GetPlayer(p-1)
+		local player = Isaac.GetPlayer(p - 1)
 		if player.FrameCount == 0 or DidPlayerCharacterJustChange(player) or DidPlayerDualityCountJustChange(player) then
 			updatePos = true
 		end
@@ -225,24 +224,24 @@ function mod:updateCheck()
 		updatePos = true
 		self.storage.numplayers = activePlayers
 	end
-	
+
 	if self.storage.hudoffset ~= Options.HUDOffset then
 		updatePos = true
 		self.storage.hudoffset = Options.HUDOffset
 	end
-		
+
 	--Was a Victory Lap Completed, Runs completed on Normal Difficulty Will switch to HARD upon start of a Victory Lap
 	if self.storage.VictoryLap ~= Game():GetVictoryLap() then
 		updatePos = true
 		self.storage.VictoryLap = Game():GetVictoryLap()
 	end
-	
+
 	--Certain Seed Effects block achievements
 	if self.storage.NumSeedEffects ~= Game():GetSeeds():CountSeedEffects() then
 		updatePos = true
 		self.storage.NumSeedEffects = Game():GetSeeds():CountSeedEffects()
 	end
-		
+
 	if updatePos then
 		self:updatePosition()
 	end
@@ -260,7 +259,7 @@ function log(text)
 end
 
 function GetMaxTrinketID()
-    return Isaac.GetItemConfig():GetTrinkets().Size -1
+	return Isaac.GetItemConfig():GetTrinkets().Size - 1
 end
 
 function DidPlayerDualityCountJustChange(player)
@@ -270,6 +269,7 @@ function DidPlayerDualityCountJustChange(player)
 	end
 	return false
 end
+
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, function(_, player)
 	local data = player:GetData()
 	local currentDualityCount = player:GetCollectibleNum(CollectibleType.COLLECTIBLE_DUALITY)
@@ -306,11 +306,11 @@ mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, function(_, player)
 end)
 
 function CanRunUnlockAchievements() -- by Xalum
-    local machine = Isaac.Spawn(6, 11, 0, Vector.Zero, Vector.Zero, nil)
-    local achievementsEnabled = machine:Exists()
-    machine:Remove()
+	local machine = Isaac.Spawn(6, 11, 0, Vector.Zero, Vector.Zero, nil)
+	local achievementsEnabled = machine:Exists()
+	machine:Remove()
 
-    return achievementsEnabled
+	return achievementsEnabled
 end
 
 function TextAcceleration(frame) --Overfit distance profile for difference text slide in
@@ -318,19 +318,19 @@ function TextAcceleration(frame) --Overfit distance profile for difference text 
 	if frame > 0 then
 		return 0
 	end
-	return -(15.1/(13*13))*frame*frame
+	return -(15.1 / (13 * 13)) * frame * frame
 end
-
 
 --init self storage from mod namespace before any callbacks by blocking.
 function mod:initStore()
-	self.storage = {} 
+	self.storage = {}
 	self.coords = Vector(21, 197.5)
 end
+
 mod:initStore()
 
 mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.updatePlanetariumChance)
-	
+
 mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.init)
 mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.exit)
 
